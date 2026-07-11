03c0317c2dd1b53f53c096c908a7a7c3a2af976f
ozgeaygul <ozge14aygul07@gmail.com>
2026-07-11 13:28:18 +0300

    Refactor SQL queries with comments and corrections

--- Assignment-1/queries.sql
+++ Assignment-1/queries.sql
@@ -1,11 +1,11 @@
 -- SELECT column FROM table;
--- film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
+-- Sort the data in the 'title' and 'description' columns of the 'film' table. (sreenshot--> select.png)
 SELECT title, description FROM film;
--- film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+-- Retrieve the data from all columns in the 'film' table where the film length is greater than 60 AND less than 75. (screenshot--> where.png)
 SELECT * FROM film WHERE length > 60 AND length < 75;
---film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
+--Retrieve the data from all columns in the `film` table where the `rental_rate` is 0.99 and the `replacement_cost` is either 12.99 or 28.99.
 SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
---customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir? 'Smith'
+--What is the value in the `last_name` column for the customer whose `first_name` column value is 'Mary' in the `customer` table? 'Smith'
 SELECT first_name, last_name FROM customer WHERE first_name = 'Mary';
---film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
+--List the data from the film table where the length is not greater than 50 and the rental_rate is neither 2.99 nor 4.99.
 SELECT * FROM film WHERE (NOT length > 50) AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
